a9320b42ec664b482492bd05df7e702e968068a4
Tran Son Hai <harefx.dev@gmail.com>
2021-03-13 14:46:56 +0700

    add test routes

--- best_quotes/config/application.rb
+++ best_quotes/config/application.rb
@@ -2,5 +2,7 @@ require "rulers"
 
 module BestQuotes
   class Application < Rulers::Application
+    $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+    require "quotes_controller"
   end
 end
